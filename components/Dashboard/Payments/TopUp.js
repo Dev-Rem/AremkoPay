00bf293e1ddcdf308335b366c11f736175bf0a0e
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2023-12-15 09:58:43 +0100

    Dashboard-TopUp: Complete Top Up Overlay

--- components/Dashboard/Payments/TopUp.js
+++ components/Dashboard/Payments/TopUp.js
@@ -1,5 +1,30 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import FormHeader from "../../utils/FormHeader";
+import { Icon } from "@rneui/themed";
 export const TopUp = ({ navigation }) => {
-  return <View>{/* <FormHeader header= */}</View>;
+  return (
+    <View>
+      <FormHeader header="NGN Account Info" subHeader="" />
+
+      <View className="mb-[5%]">
+        <Text className="text-[15%] px-[10px] text-[#939090] mb-[">
+          Bank name
+        </Text>
+        <TouchableOpacity className="bg-[#F2F2F2]  rounded-[10px] p-[10px] flex-row justify-between">
+          <Text className="text-[15%]">Wema Bank Plc</Text>
+          <Icon name={"content-copy"} type="material" size={18} />
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text className="text-[15%] px-[10px] text-[#939090] mb-[">
+          Account number
+        </Text>
+        <TouchableOpacity className="bg-[#F2F2F2]  rounded-[10px] p-[10px] flex-row justify-between">
+          <Text className="text-[15%]">2304323421</Text>
+          <Icon name={"content-copy"} type="material" size={20} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 };
--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -6,118 +6,111 @@ import {
   TouchableOpacity,
   Text,
   Keyboard,
+  ScrollView,
 } from "react-native";
 import { Icon } from "@rneui/themed";
 import { CustomListitem } from "../utils/CustomListitem";
 
 export const ProfileScreen = ({ navigation }) => {
   return (
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-    >
-      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-        <View className="flex-1 bg-[#28e068] flex-column justify-end ">
-          <View className="h-[25%] justify-end items-center px-[10%] py-[5%]">
-            <View
-              style={{
-                backgroundColor: "#f1f1f1",
-                width: "28%",
-                aspectRatio: 1,
-                borderRadius: "50%",
-              }}
-            ></View>
+    <View className="flex-1 bg-[#ffff] flex-column justify-start px-[30px] mb-[8%]">
+      <View className="h-[25%] justify-end items-center  mb-[3%]">
+        <View
+          style={{
+            backgroundColor: "#f1f1f1",
+            width: "28%",
+            aspectRatio: 1,
+            borderRadius: "50%",
+          }}
+        ></View>
 
-            <Text className="text-[20%] mb-[3%]">Aremu Oluwaseyi Festus</Text>
-            <TouchableOpacity
-              onPress={() =>
-                navigation.navigate("ProfileSecondScreen", {
-                  use: "Account Details",
-                })
-              }
-              className="flex-row  "
-            >
-              <Text className="text-[15%] text-[#FFFF]">Account details</Text>
-              <Icon
-                name={"navigate-next"}
-                type="material"
-                size={20}
-                color="#ffff"
-              />
-            </TouchableOpacity>
-          </View>
+        <Text className="text-[20%] mb-[3%]">Aremu Oluwaseyi Festus</Text>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate("ProfileSecondScreen", {
+              use: "Account Details",
+            })
+          }
+          className="flex-row  "
+        >
+          <Text className="text-[15%] text-[#28e068]">Account details</Text>
+          <Icon
+            name={"navigate-next"}
+            type="material"
+            size={20}
+            color="#28e068"
+          />
+        </TouchableOpacity>
+      </View>
 
-          <View className="bg-[#FFFF] min-h-[70%] w-[100%] rounded-tr-[50px] justify-start py-[5%] px-[20] ">
-            <CustomListitem
-              header="Change Pin"
-              subHeader="Change your login pin"
-              iconName="navigate-next"
-              iconColor="#474747"
-              headerColor="text-[#28e068]"
-              onPress={() =>
-                navigation.navigate("ProfileSecondScreen", {
-                  use: "Change Pin",
-                })
-              }
-            />
-            <CustomListitem
-              header="Update Password"
-              subHeader="Change your account password"
-              iconName="navigate-next"
-              headerColor="text-[#28e068]"
-              iconColor="#474747"
-              onPress={() =>
-                navigation.navigate("ProfileSecondScreen", {
-                  use: "Update Password",
-                })
-              }
-            />
-            <CustomListitem
-              header="Withdrawal accounts"
-              subHeader="Add or remove withdrawal accounts"
-              iconName="navigate-next"
-              headerColor="text-[#28e068]"
-              iconColor="#474747"
-              onPress={() =>
-                navigation.navigate("ProfileSecondScreen", {
-                  use: "Withdrawal Accounts",
-                })
-              }
-            />
-            <CustomListitem
-              header="Two-Factor-Authentication"
-              subHeader="Set or change your 2fa settings"
-              iconName="navigate-next"
-              headerColor="text-[#28e068]"
-              iconColor="#474747"
-              onPress={() =>
-                navigation.navigate("ProfileSecondScreen", {
-                  use: "2FA",
-                })
-              }
-            />
-            <CustomListitem
-              header="Get Help"
-              subHeader="Get support or send feedback and reveiews"
-              iconName="navigate-next"
-              headerColor="text-[#28e068]"
-              iconColor="#474747"
-              onPress={() =>
-                navigation.navigate("ProfileSecondScreen", {
-                  use: "Get Help",
-                })
-              }
-            />
-            <CustomListitem
-              header="Sign Out"
-              iconName="logout"
-              iconColor="#ef0e0e"
-              headerColor="text-[#ef0e0e]"
-              onPress={() => navigation.navigate("SignIn")}
-            />
-          </View>
-        </View>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+      {/* <View className="bg-[#FFFF] min-h-[70%] w-[100%] rounded-tr-[50px] justify-start py-[5%] px-[20] "> */}
+      <CustomListitem
+        header="Change Pin"
+        subHeader="Change your login pin"
+        iconName="navigate-next"
+        iconColor="#474747"
+        headerColor="text-[#28e068]"
+        onPress={() =>
+          navigation.navigate("ProfileSecondScreen", {
+            use: "Change Pin",
+          })
+        }
+      />
+      <CustomListitem
+        header="Update Password"
+        subHeader="Change your account password"
+        iconName="navigate-next"
+        headerColor="text-[#28e068]"
+        iconColor="#474747"
+        onPress={() =>
+          navigation.navigate("ProfileSecondScreen", {
+            use: "Update Password",
+          })
+        }
+      />
+      <CustomListitem
+        header="Withdrawal accounts"
+        subHeader="Add or remove withdrawal accounts"
+        iconName="navigate-next"
+        headerColor="text-[#28e068]"
+        iconColor="#474747"
+        onPress={() =>
+          navigation.navigate("ProfileSecondScreen", {
+            use: "Withdrawal Accounts",
+          })
+        }
+      />
+      <CustomListitem
+        header="Two-Factor-Authentication"
+        subHeader="Set or change your 2fa settings"
+        iconName="navigate-next"
+        headerColor="text-[#28e068]"
+        iconColor="#474747"
+        onPress={() =>
+          navigation.navigate("ProfileSecondScreen", {
+            use: "2FA",
+          })
+        }
+      />
+      <CustomListitem
+        header="Get Help"
+        subHeader="Get support or send feedback and reveiews"
+        iconName="navigate-next"
+        headerColor="text-[#28e068]"
+        iconColor="#474747"
+        onPress={() =>
+          navigation.navigate("ProfileSecondScreen", {
+            use: "Get Help",
+          })
+        }
+      />
+      <CustomListitem
+        header="Sign Out"
+        iconName="logout"
+        iconColor="#ef0e0e"
+        headerColor="text-[#ef0e0e]"
+        onPress={() => navigation.navigate("SignIn")}
+      />
+    </View>
   );
 };
